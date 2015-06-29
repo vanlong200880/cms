5f7bddbd9422ee5fb8cf4692aec743a5f6faf79c
long.pham <vanlong200880@gmail.com>
2015-06-29 23:16:24 +0700

    add html product

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -73,7 +73,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Product' => 'Application\Controller\ProductController'
         ),
     ),
     'view_manager' => array(
