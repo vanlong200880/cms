e7d4fc56f89b1fe71678b92a4d0047de79dd0081
long.pham <vanlong200880@gmail.com>
2015-08-24 23:23:49 +0700

    html

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,25 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=cms;host=localhost;charset=utf8',
+        'driver_option' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND  => "SET NAMES UTF8",
+        ),
+        'username' => 'root',
+        'password' => '',
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',          
+//            'Zend\Db\Adapter\Adapter' => function ($serviceManager) {
+//            $adapterFactory = new Zend\Db\Adapter\AdapterServiceFactory();
+//               $adapter = $adapterFactory->createService($serviceManager);
+//               \Zend\Db\TableGateway\Feature\GlobalAdapterFeature::setStaticAdapter($adapter);
+//               return $adapter;
+//         }
+        ),
+    ),
+    'static_salt' => 'Sfwf232DGWEGDGSA',
 );
